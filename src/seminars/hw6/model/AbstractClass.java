0c8757c9e02595e2fccfdf00bac9e57a83762169
Andrey Slobodchikov <andslob@inbox.ru>
2023-10-13 13:54:48 +0500

    done

---
+++ src/seminars/hw6/model/AbstractClass.java
@@ -0,0 +1,11 @@
+package seminars.hw6.model;
+
+public abstract class AbstractClass {
+    int[] array;
+
+    double resultAverage;
+
+    int[] createArray() {
+        return new int[10];
+    }
+}
---
+++ src/seminars/hw6/model/ListInterface.java
@@ -0,0 +1,9 @@
+package seminars.hw6.model;
+
+public interface ListInterface {
+
+    void compareArray(double resultAverage1, double resultAverage2);
+
+    double averageArray(int[] array);
+
+}
